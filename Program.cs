38696bf7955feed95faf840e2327895f3437d523
machine <analyticrye@gmail.com>
2021-10-11 22:54:33 -0400

    refactored. Bug with healing dice  having the same states

--- Program.cs
+++ Program.cs
@@ -8,26 +8,31 @@ namespace DnDDiceRollingApp
         {
             bool isRunning = true;
 
+            Console.WriteLine("***DnD Dice Rolling App***");
             do
             {
-                System.Console.WriteLine("DnD Dice Rolling App");
+                DisplayDice();
+                RollAgain();
+            }
+            while (isRunning);
 
-                System.Console.WriteLine("Enter D20, D12, D10, D8, D6, or D4 to roll your die! Enter H, GH, SH, or SUPH for heal rolls.");
+            void DisplayDice()
+            {
+
+                Console.WriteLine("Enter D20, D12, D10, D8, D6, or D4 to roll your die! Enter H, GH, SH, or SUPH for heal rolls.");
                 string inputDice = Console.ReadLine().ToUpper();
                 string selectedDice = SelectDice.DiceResult(inputDice);
-                System.Console.WriteLine(selectedDice);
-
-
-
-                System.Console.WriteLine("Do you want to quit or roll again? Type Q to quit or any key to roll again.");
+                Console.WriteLine(selectedDice);
+            }
+            void RollAgain()
+            {
+                Console.WriteLine("Do you want to quit or roll again? Type Q to quit or any key to roll again.");
                 string inputQuit = Console.ReadLine();
                 if (inputQuit.ToLower() == "q")
                 {
                     isRunning = false;
                 }
-
             }
-            while (isRunning);
         }
     }
 }
